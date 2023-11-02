8c1e29af987bcbb10d63296cc211261d9d008618
Awointa <akosawointa@gmail.com>
2023-11-02 12:33:30 +0100

    added props to get the value of the rating from the StarRating component

--- src/StarRating.jsx
+++ src/StarRating.jsx
@@ -25,12 +25,14 @@ export default function StarRating({
 	color = "#fcc419",
 	size = 48,
 	defaultRating = 0,
+	onSetRating,
 }) {
 	const [rating, setRating] = useState(defaultRating);
 	const [tempRating, setTempRating] = useState(0);
 
 	function handleRating(rating) {
 		setRating(rating);
+		onSetRating(rating);
 	}
 
 	const textStyle = {
