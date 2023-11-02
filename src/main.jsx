80646a1c94c71f47fb4ce0761447a8207a0e8f58
Awointa <akosawointa@gmail.com>
2023-11-02 12:38:36 +0100

    Deleted temporary import of the StarRating component

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-// import StarRating from "./StarRating";
+
+function Test() {}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<App />
-		{/*<StarRating
-			maxRating={5}
-			messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
-/>*/}
 	</React.StrictMode>,
 );
